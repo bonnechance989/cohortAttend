d102922bbe05d36c4f70732a1a9354fa153a9298
bonnechance989 <109989716+bonnechance989@users.noreply.github.com>
2023-11-10 12:33:10 -0900

    changes

--- cohortAttend/force-app/main/default/lwc/attendanceCounter/attendanceCounter.js
+++ cohortAttend/force-app/main/default/lwc/attendanceCounter/attendanceCounter.js
@@ -1,17 +1,8 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AttendanceCounter extends LightningElement {
-    @api totalAttendance = 0;  // Assuming totalAttendance is a number
+    @api totalAttendance = 0;  
     @api attendanceRecords = [];
-
-    connectedCallback() {
-        // Hardcode some sample data for testing
-        this.attendanceRecords = [
-            { date: '2023-01-01', totalAttendance: 10 },
-            { date: '2023-01-02', totalAttendance: 15 },
-            // Add more records as needed
-        ];
-    }
 
     get sortedRecords() {
         console.log('Sorting attendance records:', this.attendanceRecords);
