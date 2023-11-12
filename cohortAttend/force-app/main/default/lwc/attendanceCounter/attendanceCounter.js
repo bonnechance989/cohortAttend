29538d8cbcd8664c1e5a2838a0ad66074696fe41
bonnechance989 <109989716+bonnechance989@users.noreply.github.com>
2023-11-11 16:27:49 -0900

    working on basic attendance for choice cohort

--- cohortAttend/force-app/main/default/lwc/attendanceCounter/attendanceCounter.js
+++ cohortAttend/force-app/main/default/lwc/attendanceCounter/attendanceCounter.js
@@ -4,14 +4,6 @@ export default class AttendanceCounter extends LightningElement {
     @api totalAttendance = 0;  
     @wire(getWorshipServiceRecords) attendanceRecords;
 
-    // get sortedRecords() {
-    //     console.log('Sorting attendance records:', this.attendanceRecords);
-    //     if (Array.isArray(this.attendanceRecords)) {
-    //         return this.attendanceRecords.slice().sort((a, b) => new Date(b.date) - new Date(a.date));
-    //     }
-    //     return [];
-    // }
-
     get sortedRecords() {
         if (this.attendanceRecords.data) {
             return this.attendanceRecords.data.map(record => ({
